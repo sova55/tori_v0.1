38919833b18e683770f3e3b6eec573776eb3ed7a
Eero Sova <t7soee00@students.oamk.fi>
2020-10-01 02:39:19 +0300

    Login done

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const router = express.Router();
+const app = express();
+const mongoose = require('mongoose');
+const expressEjsLayout = require('express-ejs-layouts')
+const session = require('express-session');
+const flash = require('connect-flash');
+const passport = require('passport');
+require("./config/passport")(passport)
+
+//mongoose
+mongoose.connect('mongodb://localhost/test',{useNewUrlParser: true, useUnifiedTopology : true})
+.then(() => console.log('connected,,'))
+.catch((err)=> console.log(err));
+//EJS
+app.set('view engine','ejs');
+app.use(expressEjsLayout);
+//express session
+app.use(session({
+    secret : 'secret',
+    resave : true,
+    saveUninitialized : true
+   }));
+    app.use(passport.initialize());
+    app.use(passport.session());
+   //use flash
+   app.use(flash());
+   app.use((req,res,next)=> {
+     res.locals.success_msg = req.flash('success_msg');
+     res.locals.error_msg = req.flash('error_msg');
+     res.locals.error  = req.flash('error');
+   next();
+   })
+//BodyParser
+app.use(express.urlencoded({extended : false}));
+
+//Routes
+app.use('/',require('./routes/index'));
+app.use('/users',require('./routes/users'));
+
+app.listen(3000); 
